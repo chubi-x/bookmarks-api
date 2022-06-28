e148370b12f177747d79d8fb587bcd2982786b00
chubi-x <michaeladejoh01@gmail.com>
2022-06-29 00:07:55 +0100

    feat: create signup and login endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController{
     constructor(private authService: AuthService){}
+
+    // create sign up and sign in endpoints
+    @Post('signup')
+    signup(){
+        return 'sign up route'
+    }
+
+    @Post('signin')
+    signin(){
+        return 'sign in route'
+    }
 }
