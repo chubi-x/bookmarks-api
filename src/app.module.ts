fa3350187278968d502d69d23dfea90dd8f42314
chubi-x <michaeladejoh01@gmail.com>
2022-06-28 23:44:11 +0100

    chore: import auth module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
-
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [UserModule, BookmarkModule],
+  imports: [UserModule, BookmarkModule,AuthModule],
 })
 export class AppModule {}
