233f3f0228e4805dc5b202eba896f0ee05378ec5
chubi-x <michaeladejoh01@gmail.com>
2022-07-01 10:27:18 +0100

    feat: give auth module access to prisma db module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import {Module} from '@nestjs/common'
+import { PrismaDbModule } from 'src/prisma_db/prisma_db.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-    imports: [AuthModule],
+    imports: [PrismaDbModule],
     controllers: [AuthController],
     providers: [AuthService]
 })
