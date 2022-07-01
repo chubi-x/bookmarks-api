e1c53263dc1444aaacec4071a0b5a6ffa13e3e4d
chubi-x <michaeladejoh01@gmail.com>
2022-07-01 12:10:31 +0100

    feat: configure auth controller to use dto to specify req body shape

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { Request } from "express";
+import { AuthDto } from "./dto";
 @Controller('auth')
 export class AuthController{
     constructor(private authService: AuthService){}
 
     // create sign up and sign in endpoints
     @Post('signup')
-    signup(@Body() dto:any){
+    signup(@Body() dto:AuthDto){
         console.log({dto})
         return this.authService.signup();
     }
