7dd47ca527212b6e1f82f35ab09a22b3138337e3
chubi-x <michaeladejoh01@gmail.com>
2022-07-04 14:57:49 +0100

    feat: handle errors where user credentials might be taken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaDbService } from "src/prisma_db/prisma_db.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 @Injectable()
 export class AuthService{
     constructor(private prisma: PrismaDbService){
@@ -15,7 +16,8 @@ export class AuthService{
         //generate password hash
         const hash = await argon.hash(dto.password);
         //save new user in db
-        const user = await this.prisma.user.create({
+        try{
+            const user = await this.prisma.user.create({
             data: {
                 email: dto.email,
                 hash
@@ -28,5 +30,14 @@ export class AuthService{
         })
         //return saved user
         return user;
+        }
+        catch(err){
+            if (err instanceof PrismaClientKnownRequestError){
+                if (err.code === "P2002"){
+                    throw new ForbiddenException('Credentials are taken')
+                }
+            }
+            throw err
+        }
     }
 }
