b1d5289b5263c4251e68f6f8528a0cc18341e8e9
chubi-x <michaeladejoh01@gmail.com>
2022-07-04 15:18:06 +0100

    feat: complete sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,40 +4,59 @@ import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 @Injectable()
-export class AuthService{
-    constructor(private prisma: PrismaDbService){
-        
-    }
-    signin(){
-        
-        return {msg: 'sign me in scotty'};
-    }
-    async signup(dto: AuthDto){
-        //generate password hash
-        const hash = await argon.hash(dto.password);
-        //save new user in db
-        try{
-            const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash
-            },
-            select: {
-                id: true,
-                createdAt: true,
-                email: true
-            }
-        })
-        //return saved user
-        return user;
+export class AuthService {
+  constructor(private prisma: PrismaDbService) {}
+
+  /**
+   * SIGN IN LOGIC
+   * */
+  async signin(dto: AuthDto) {
+    //find the user using their email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //if user does not exist throw exception (called a guard exception)
+    if (!user) throw new ForbiddenException('User does not exist');
+    // compare user password hash to password provided by user
+    const pwMatches = await argon.verify(user.hash, dto.email);
+    // if the passwords dont match throw an exception
+    if (!pwMatches) throw new ForbiddenException('password does not match');
+
+    //if user exists and password is correct send back the user
+    delete user.hash;
+    return user;
+  }
+
+  /**
+   * SIGN UP LOGIC
+   * */
+  async signup(dto: AuthDto) {
+    //generate password hash
+    const hash = await argon.hash(dto.password);
+    //save new user in db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          createdAt: true,
+          email: true,
+        },
+      });
+      //return saved user
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credentials are taken');
         }
-        catch(err){
-            if (err instanceof PrismaClientKnownRequestError){
-                if (err.code === "P2002"){
-                    throw new ForbiddenException('Credentials are taken')
-                }
-            }
-            throw err
-        }
+      }
+      throw err;
     }
+  }
 }
