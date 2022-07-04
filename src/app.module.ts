7f7cc899ddc041e1c1c5f1fb826e87f662301511
chubi-x <michaeladejoh01@gmail.com>
2022-07-04 17:49:32 +0100

    feat: import config module to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,13 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaDbModule } from './prisma_db/prisma_db.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule, BookmarkModule,AuthModule, PrismaDbModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    UserModule, 
+    BookmarkModule,
+    AuthModule, 
+    PrismaDbModule],
 })
 export class AppModule {}
