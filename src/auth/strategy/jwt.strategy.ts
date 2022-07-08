9c2ee1ac72bb5a2f7af730035428e60395a9a153
chubi-x <chubixax@protonmail.com>
2022-07-08 12:25:45 +0100

    feat: validate user with jwt token and return payload

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,11 +3,19 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  // extract jwt token from header and jwt secret from environment using config service
   constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+  // method to validate a users with their token
+  validate(payload: any) {
+    console.log({
+      payload,
+    });
+    return payload;
   }
 }
