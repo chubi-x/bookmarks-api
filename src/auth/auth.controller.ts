f98d0b5cfe5cef13a454debdeb52c79bf52576da
chubi-x <chubixax@protonmail.com>
2022-07-08 15:31:42 +0100

    chore; specify 200 code upon signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  // create sign up and sign in endpoints
+  /**
+   * CREATE SIGN UP AND SIGN IN ENDPOINTS*/
+
+  // sign up endpoint
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
-
+  // sign in endpoint
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
