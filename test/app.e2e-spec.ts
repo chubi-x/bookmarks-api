28ab2ef1640b4116fe01a63e204a8e70d4db2819
chubi-x <chubixax@protonmail.com>
2022-07-08 16:57:19 +0100

    feat: create the testing nest application

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,16 @@
+import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 describe('App e2e', () => {
+  // create app variable
+  let app: INestApplication;
   // mount the app module
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    // create the nest application
+    app = moduleRef.createNestApplication();
   });
   it.todo('should pass');
 });
