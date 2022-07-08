b00121b2bd3fa3f122889b1a478d59a1de471c44
chubi-x <chubixax@protonmail.com>
2022-07-08 12:54:51 +0100

    feat: use guard on user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,9 +3,9 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
