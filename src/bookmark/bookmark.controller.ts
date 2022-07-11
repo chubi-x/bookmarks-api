17a100520ded409c587e5c6d366746c7b8e20da8
chubi-x <chubixax@protonmail.com>
2022-07-11 19:06:37 +0100

    feat: CRUD functions for bookmarks

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { BookmarkService } from './bookmark.service';
 
-@Controller('bookmark')
-export class BookmarkController {}
+@Controller('bookmarks')
+export class BookmarkController {
+  constructor(private bookmark: BookmarkService) {}
+  @UseGuards(JwtGuard)
+  @Get()
+  getBookmarks() {}
+  @Get()
+  getBookmarkById() {}
+
+  @Patch()
+  editBookmarkById() {}
+
+  @Post()
+  createBookmark() {}
+  @Delete()
+  deleteBookmarkById() {}
+}
