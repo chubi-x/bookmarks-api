9517ba8200cde492dc854d7ce0b05b2b7f5d8a7b
chubi-x <chubixax@protonmail.com>
2022-07-11 09:50:41 +0100

    feat: write sign up test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { PrismaDbService } from 'src/prisma_db/prisma_db.service';
+import * as pactum from 'pactum';
+import { PrismaDbService } from '../src/prisma_db/prisma_db.service';
 import { AppModule } from '../src/app.module';
+import { AuthDto } from 'src/auth/dto';
 describe('App e2e', () => {
   // create app variable
   let app: INestApplication;
@@ -16,12 +18,33 @@ describe('App e2e', () => {
     // use global pipes
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    // start the server
+    await app.listen(3333);
     prisma = app.get(PrismaDbService);
-    prisma.cleanDb();
+    await prisma.cleanDb();
   });
   // close the app after running all tests
   afterAll(() => {
     app.close();
   });
-  it.todo('should pass');
+  // authentication tests
+  describe('Auth', () => {
+    describe('Sign Up', () => {
+      it('should sign up', () => {
+        // create dto object to test sign up
+        const dto: AuthDto = {
+          email: 'chubi1234@gmail.com',
+          password: '12345',
+          firstName: 'chubiyojo',
+          lastName: 'adejoh',
+        };
+        return pactum
+          .spec()
+          .post('http://localhost:3333/auth/signup')
+          .withBody(dto)
+          .expectStatus(201)
+          .inspect();
+      });
+    });
+  });
 });
