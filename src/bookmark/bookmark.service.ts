8165c06c3449de56717dc925fb9eee78591fdc5c
chubi-x <chubixax@protonmail.com>
2022-07-11 19:07:44 +0100

    feat: populate bookmark service with CRUD functions

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaDbService } from 'src/prisma_db/prisma_db.service';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  constructor(private prisma: PrismaDbService) {}
+  getBookmarks() {}
+  getBookmarkById() {}
+
+  editBookmarkById() {}
+
+  createBookmark() {}
+  deleteBookmarkById() {}
+}
