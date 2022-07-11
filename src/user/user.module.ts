5d83e99680aa5265f00af515f04826a46f73b5f3
chubi-x <chubixax@protonmail.com>
2022-07-11 14:39:53 +0100

    bug: import user service into module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 // user module
 @Module({
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
